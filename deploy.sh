f6f8328fd366145ad0e80b673f1962bcc9e20714
guanjianhui <guanjianhui@xhqb.com>
2019-02-27 18:35:02 +0800

    S3_PREFIX config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 
-S3_PREFIX="serverless"
+S3_PREFIX="${S3_PREFIX:-serverless}"
 
 sam package --s3-bucket $S3_BUCKET --s3-prefix $S3_PREFIX \
   --template-file template.yaml --output-template-file target/packaged.yaml
